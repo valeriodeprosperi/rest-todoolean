99cf9200e38206872eddafda54025cfda808b4c1
valerio <valerio.deprosperi@gmail.com>
2020-09-28 15:57:18 +0200

    js read GET

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// L'esercizio di oggi è quello di creare, come fatto in aula, una todo list sulla quale sarà possibile svolgere le operazioni di CRUD, usando qusta Api: http://157.230.17.132:XXXX/todos
+
+$(document).ready(function() {
+
+
+  // ready
+
+$.ajax(
+{
+  "url": "http://157.230.17.132:3019/todos",
+  "method": "GET",
+  "success": function(data) {
+    render(data);
+  },
+  "error": function() {
+    alert("error");
+  }
+
+
+});
+
+
+
+
+
+
+});
